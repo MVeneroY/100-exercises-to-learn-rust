040fe1b21e50839b3201d0b21713b6f5ff9f990e
MVY <veneromiguel337@gmail.com>
2025-03-11 10:10:37 -0400

    vec

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,17 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut fib: Vec<u32> = vec![0,1];
+
+    if n <= 1 {
+        return fib[n as usize];
+    }
+    
+    for i in 0..n-1 {
+        fib.push(fib[i as usize] + fib[(i+1) as usize])
+    }
+
+    fib[n as usize]
 }
 
 #[cfg(test)]
